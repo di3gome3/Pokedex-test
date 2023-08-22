ad921ebaecce3ec85c752fa6e94083100109b9dd
Diego <santosd630@gmail.com>
2023-08-22 20:01:34 -0300

    First Version

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -33,7 +33,33 @@ const getPokemons = async (id) => {
 }
 
 const createPokemonCard = (poke) => {
+    const card = document.createElement('div')
+    card.classList.add('pokemon')
+
+    const name = poke.name[0].toUpperCase() + poke.name.slice(1)
+    const id = poke.id.toString().padStart(3, 0)
+
+    const pokeTypes = poke.types.map(type => type.type.name)
+    const type = mainTypes.find(type => pokeTypes.indexOf(type) > -1)
+    const color = colors[type]
+
+    card.style.backgroundColor = color
     
+    const pokemonInnerHTML = `<div class="imgContainer">
+    <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${poke.id}.png"
+      alt="${name}"
+    />
+  </div>
+  <div class="info">
+    <span class="number">#${id}</span>
+    <h3 class="name">${name}</h3>
+    <small class="type">Type: <span>${type}</span></small>
+  </div>`
+
+  card.innerHTML = pokemonInnerHTML
+
+  conteiner.appendChild(card)
 }
 
 fetchPokemons()
