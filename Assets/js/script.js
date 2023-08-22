861c92c743c23386681a412ead23128a2654df1d
Diego <santosd630@gmail.com>
2023-08-22 19:38:35 -0300

    first commit

---
+++ Assets/js/script.js
@@ -0,0 +1,39 @@
+const conteiner = document.querySelector('.container')
+const pokemonCount = 151
+const colors = {
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#F5F5F5',
+    fighting: '#E6E0D4',
+    normal: '#F5F5F5'
+}
+
+const mainTypes = Object.keys(colors)
+
+const fetchPokemons = async () => {
+    for (let i = 1; i <= pokemonCount; i++){
+        await getPokemons(i)
+    }
+}
+
+const getPokemons = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const resp = await fetch(url)
+    const data = await resp.json()
+    createPokemonCard(data)
+}
+
+const createPokemonCard = (poke) => {
+    
+}
+
+fetchPokemons()
